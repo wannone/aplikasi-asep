dbe36d5d0979a31a6c3cec33a000e32664677840
Awan Abdillah <awanabdill@gmail.com>
2026-09-04 20:31:25 +0700

    Initial commit

---
+++ aplikasi-asep/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace aplikasi_asep.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
